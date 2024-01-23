34cf816a742571f0b0cf807fa29be01cc2827e59
JyotiRekhaGiri <107474272+JyotiRekhaGiri@users.noreply.github.com>
2024-01-23 11:56:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function submitForm(){
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getELementById("lastName").value;
+	var phoneNum = document.getElementById("phoneNumber").value;
+	var emailId = document.getElementById("email").value;
+
+	var alertMessage = "First Name:"+firstName+ "\n"+"Last Name:"+ lastName+ "\n"+
+		"Phone Number:"+ phoneNum+"\n"+"Email ID:"+emailId+"\n";
+
+	alert(alertMessage);
+	document.getElementById("myForm").reset();
+	document.querySelector("button").style.backgroundColor="blue";
+}
